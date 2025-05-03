1bc0096a8a51f0b7a81d8d059c927af55d7dfdb4
Steffen Schinke <steffen.schinke@gmail.com>
2025-05-04 00:07:27 +0200

    components: async image, nav back button

---
+++ src/main/java/de/schinke/steffen/ui/components/AsyncImage.kt
@@ -0,0 +1,77 @@
+package de.schinke.steffen.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrokenImage
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import kotlin.Int
+
+
+@Composable
+fun AsyncImage(url: String, size: Dp = 80.dp) {
+
+    val imageSize = (size.value * LocalDensity.current.density).toInt()
+    val painter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(url)
+            .crossfade(true)
+            .size(imageSize)
+            .build()
+    )
+    val state = painter.state
+
+    Box(
+        modifier = Modifier
+            .size(size)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(4.dp),
+        contentAlignment = Alignment.Center
+    ) {
+
+        when (state) {
+
+            is AsyncImagePainter.State.Loading -> {
+
+                CircularProgressIndicator(modifier = Modifier.size(24.dp))
+            }
+            is AsyncImagePainter.State.Success -> {
+
+                Image(
+                    painter = painter,
+                    contentDescription = "Cover",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            is AsyncImagePainter.State.Error-> {
+
+                Icon(Icons.Default.BrokenImage, contentDescription = "Error")
+            }
+            else -> {
+
+                Icon(Icons.Default.BrokenImage, contentDescription = "Error")
+            }
+        }
+    }
+}
