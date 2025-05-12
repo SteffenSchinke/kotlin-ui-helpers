610ffd09e2e8e3f8fb637920660c44494b080e66
Steffen Schinke <steffen.schinke@gmail.com>
2025-05-12 21:03:51 +0200

    code review:  interface app screen content  tasks screen

--- src/main/java/de/schinke/steffen/interfaces/AppScreenContent.kt
+++ src/main/java/de/schinke/steffen/interfaces/AppScreenContent.kt
@@ -2,8 +2,6 @@ package de.schinke.steffen.interfaces
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import de.schinke.steffen.base_classs.AppBaseViewModel
-import de.schinke.steffen.enums.ViewModelState
 
 interface AppScreenContent {
 
