0e26f9d80f74f320f94f11e87061d0093100b704
Steffen Schinke <steffen.schinke@gmail.com>
2025-05-19 23:49:55 +0200

    code review

--- src/main/java/de/schinke/steffen/ui/components/CustomAsyncImage.kt
+++ src/main/java/de/schinke/steffen/ui/components/CustomAsyncImage.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -74,7 +75,8 @@ fun CustomAsyncImage(
                 Image(
                     painter = painter,
                     contentDescription = "Cover",
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
                 )
             }
             is AsyncImagePainter.State.Error -> {
