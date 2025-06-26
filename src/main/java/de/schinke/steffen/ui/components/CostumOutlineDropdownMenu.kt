48cad9c066928ef15d4938acb0d19ae5d278d925
Steffen Schinke <steffen.schinke@gmail.com>
2025-06-26 20:19:05 +0200

    outline dropdown menu

--- src/main/java/de/schinke/steffen/ui/components/CostumOutlineDropdownMenu.kt
+++ src/main/java/de/schinke/steffen/ui/components/CostumOutlineDropdownMenu.kt
@@ -10,15 +10,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
